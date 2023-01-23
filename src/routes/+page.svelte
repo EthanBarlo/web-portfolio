0528477c5aa41792f2d89fc1738e57ef7d885164
Ethan Barlow <ethanbar266@gmail.com>
2023-01-24 00:09:26 +0100

    Updated main page a bit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,32 +3,37 @@
 </script>
 
 <main class="font-charm">
-	<div class="grid grid-cols-2 border-b-2 border-lavendar-900 overflow-hidden">
+	<div class="flex flex-col-reverse sm:flex-row border-b-2 border-lavendar-900 overflow-hidden">
 		<section>
-			<h1 class="text-3xl">Hey there!</h1>
-			<br />
+			<h1 class="text-3xl mb-2 mt-1">Hey there!</h1>
 			<p>
 				I'm a Software Engineer studying at Sheffield Hallam University, Currently working as a
 				Full-Stack Developer at <a
 					href="https://www.bemoresocial.co.uk"
-					class="text-lavender-900 transition-all relative overflow-hidden after:w-full after:h-[0.10rem] after:content-[''] after:bg-lavender-900 after:absolute after:bottom-0 after:-left-full hover:after:translate-x-full after:transition-all"
+					class="text-lavender-800 transition-all relative overflow-hidden after:w-0 after:h-[0.10rem]  after:bg-lavender-600 after:absolute after:bottom-0 after:left-0 hover:after:w-full after:transition-all"
 					>BeMoreSocial</a
 				>.
 			</p>
 			<p>
 				Throughout this website you will find a selection of my projects and experiences that
-				showcase my skills and interest in Software Development. Each project demonstrates my
-				ability to solve problems and develop high-quality code.
+				showcase my skills and interest in Software Development. Although I might have a pretty
+				website, I am not a UI designer. So please excuse any oddities you may find.
 			</p>
 		</section>
 		<section>
-			<img class="rounded" src="/images/headshot-transparent.png" alt="" />
+			<img
+				class="rounded m-auto hidden sm:block"
+				src="/images/headshot-transparent.webp"
+				width="1200"
+				height="1200"
+				alt=""
+			/>
 		</section>
 	</div>
 
 	<p>
 		Throughout my studies, personal projects and work, I have developed a strong understanding in
-		programming languages such as:
+		the following programming languages and frameworks:
 	</p>
 	<Tags
 		data={[
@@ -38,7 +43,6 @@
 			'C# / Rust / Java'
 		]}
 	/>
-
 	<p>
 		I am always looking for new opportunities to learn and grow as a Software Engineer. Please don't
 		hesitate to reach out to me if you have any questions or would like to collaborate on a project.
