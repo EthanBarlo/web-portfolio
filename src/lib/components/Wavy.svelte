c1ee87641318be2b21c66f4fcdd56355f40143a8
Ethan Barlow <ethanbar266@gmail.com>
2023-02-01 18:03:12 +0100

    Created Youtube & Wavy components

---
+++ src/lib/components/Wavy.svelte
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<div
+	class="wavy-wrapper -ml-6 w-[calc(100%+3rem)] -my-3 bg-gradient-to-r from-transparent to-lavender-400"
+>
+	<div class="content py-14 mx-6">
+		<slot />
+	</div>
+</div>
+
+<style>
+	.wavy-wrapper {
+		position: relative;
+		--mask: radial-gradient(60.47px at 50% 82.5px, #000 99%, #0000 101%) calc(50% - 60px) 0/120px
+				51% repeat-x,
+			radial-gradient(60.47px at 50% -52.5px, #0000 99%, #000 101%) 50% 30px/120px calc(51% - 30px)
+				repeat-x,
+			radial-gradient(60.47px at 50% calc(100% - 82.5px), #000 99%, #0000 101%) calc(50% - 60px)
+				100%/120px 51% repeat-x,
+			radial-gradient(60.47px at 50% calc(100% + 52.5px), #0000 99%, #000 101%) 50%
+				calc(100% - 30px) / 120px calc(51% - 30px) repeat-x;
+		-webkit-mask: var(--mask);
+		mask: var(--mask);
+		overflow: hidden;
+	}
+</style>
---
+++ src/lib/components/YouTube-Video.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let videoId: string;
+	export let title: string;
+</script>
+
+<iframe
+	class="w-full aspect-video overflow-hidden rounded-lg"
+	src={`https://www.youtube.com/embed/${videoId}`}
+	{title}
+	frameborder="0"
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+	allowfullscreen
+/>
